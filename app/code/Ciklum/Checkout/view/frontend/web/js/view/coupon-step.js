4c78c2fd72bdbd0950bcf3a061270fadacb98858
Georgiy <goshazvir@gmail.com>
2018-11-13 00:26:02 +0200

    + clean up;

--- app/code/Ciklum/Checkout/view/frontend/web/js/view/coupon-step.js
+++ app/code/Ciklum/Checkout/view/frontend/web/js/view/coupon-step.js
@@ -38,15 +38,7 @@ define(
                     null,
                     this.stepTitle,
                     this.isVisible,
-
                     _.bind(this.navigate, this),
-
-                    /**
-                     * sort order value
-                     * 'sort order value' < 10: step displays before shipping step;
-                     * 10 < 'sort order value' < 20 : step displays between shipping and payment step
-                     * 'sort order value' > 20 : step displays after payment step
-                     */
                     15
                 );
 
@@ -54,27 +46,14 @@ define(
             },
 
             /**
-             * The navigate() method is responsible for navigation between checkout step
-             * during checkout. You can add custom logic, for example some conditions
-             * for switching to your custom step
-             */
-            navigate: function () {
-
-            },
-
-            /**
             * Form submit handler
             */
             onSubmit: function() {
-                // trigger form validation
                 this.source.set('params.invalid', false);
                 this.source.trigger('customCheckoutForm.data.validate');
 
-                // verify that form data is valid
                 if (!this.source.get('params.invalid')) {
-                    // data is retrieved from data provider by value of the customScope property
                     var formData = this.source.get('customCheckoutForm');
-                    // do something with form data
                     console.dir(formData);
                 }
             },
