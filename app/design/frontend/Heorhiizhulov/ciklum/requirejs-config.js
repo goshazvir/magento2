231d8a1d684d7c98a9cb9fdf43c47ea3bf6cdbf5
Georgiy <goshazvir@gmail.com>
2018-12-25 13:39:44 +0200

    + upd path;

--- app/design/frontend/Heorhiizhulov/ciklum/requirejs-config.js
+++ app/design/frontend/Heorhiizhulov/ciklum/requirejs-config.js
@@ -1,27 +1,28 @@
 var config = {
-    deps: ['Magento_Cms/js/log-when-loaded'],
+    deps: ['Magento_Cms/js/require/log-when-loaded'],
 
     shim: {
-        'Magento_Catalog/js/view/compare-products': {
-            deps: ['Magento_Cms/js/before-compare-products-example']
+        'Magento_Catalog/js/require/view/compare-products': {
+            deps: ['Magento_Cms/js/require/before-compare-products-example']
         }
     },
 
     map: {
         '*': {
-            coffee: 'Magento_Cms/js/requirejs-example'
+            coffee: 'Magento_Cms/js/require/requirejs-example'
         }
     },
 
     config: {
         mixins: {
-            'Magento_Checkout/js/checkout-data': {
-                'Magento_Cms/js/checkout-data-mixin': true
+            'Magento_Checkout/js/require/checkout-data': {
+                'Magento_Cms/js/require/checkout-data-mixin': true
             }
         }
     },
 
     paths: {
-        'cat_title': 'Magento_Cms/js/title/v2'
+        'cat_title': 'Magento_Cms/js/require/title/v2'
+        //'cat_title2': 'Magento_Cms/js/require/title/v1'
     }
 };
