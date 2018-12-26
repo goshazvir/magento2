49d9fc4fb61c932a61c904d84befd2b432efb4e8
Georgiy <goshazvir@gmail.com>
2018-12-26 16:14:59 +0200

    add layout settings;

---
+++ app/design/frontend/Heorhiizhulov/ciklum/Magento_Cms/web/js/ui-components/layout-b.js
@@ -0,0 +1,10 @@
+define(['uiComponent'], function (Component) {
+    'use strict';
+
+    return Component.extend({
+        defaults: {
+            title: 'Layout Component B',
+            value: 5.5
+        }
+    });
+});
